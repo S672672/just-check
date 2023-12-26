0348a36b1521ff72beb4d936a4f210565d2a8239
RedHorseAatman <smith@localhost.localdomain>
2023-12-26 22:03:46 +0530

    added a file that contains a object

---
+++ newFile.js
@@ -0,0 +1,11 @@
+function newFun(){
+    const myIntro = {
+        Name : "Smith BHAttarai",
+        age : 22,
+        study : "btech",
+        branch : "computer engineering",
+    }
+    console.log(myIntro);
+    console.log(typeof(myIntro))
+}
+newFun();
